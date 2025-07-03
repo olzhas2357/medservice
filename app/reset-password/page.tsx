2db58f6de03c67268c45281eb9553d235bcb256c
Olzhas <olzhas.koshkarbay@gmail.com>
2025-07-04 00:35:10 +0500

    connection to firebase4

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -5,9 +5,7 @@ import ResetPasswordForm from "./ResetPasswordForm";
 
 export default function ResetPasswordPage() {
   return (
-      <Suspense fallback={
-        <div className="p-8 text-center">Загрузка...</div>
-      }>
+      <Suspense>
         <ResetPasswordForm />
       </Suspense>
   );
